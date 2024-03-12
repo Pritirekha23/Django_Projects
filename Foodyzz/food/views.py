139ac87a7da1cc12e0f0fd5f1433a7aa402c01d9
Pritirekha panda <learnpriti23@gmail.com>
2024-03-13 00:46:59 +0530

    third commit

--- Foodyzz/food/views.py
+++ Foodyzz/food/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render,HttpResponse
+from .models import Item
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("HELLO WORLD")
+    item_list=Item.objects.all()
+    return HttpResponse(item_list)
 
 def item(request):
     return HttpResponse("<h1>Hii PRITIREKHA</h1>")
